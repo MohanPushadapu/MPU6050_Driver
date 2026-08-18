72e891f6810cbc32d722fd943e94c3a9401b47c3
MohanPushadapu <mohanpushadapu1@gmail.com>
2026-08-19 02:45:37 +0530

    Added calculated values functions.

--- include/mpu6050/mpu6050.hpp
+++ include/mpu6050/mpu6050.hpp
@@ -6,7 +6,6 @@
 #include "registers.hpp"
 #include "namespace.hpp"
 
-
 class MPU6050
 {
 public:
@@ -25,34 +24,34 @@ public:
     AccelRange getAccelRange();
     GyroRange getGyroRange();
 
-    Error readRawAccel(Vector3i16& accel);
-    Error readRawGyro(Vector3i16& gyro);
-    Error readTemperature(int16_t& temperature);
-    Error readAll(IMUData& data);
+    Error readRawAccel(Vector3i16 &accel);
+    Error readRawGyro(Vector3i16 &gyro);
+    Error readTemperature(int16_t &temperature);
+    Error readRawAll(IMURawData &data);
+
+    Error readAccel(Vector3f &accel);
+    Error readGyro(Vector3f &gyro);
+    Error readAll(IMURawData &data);
 
     Error enableFIFO(bool enable);
     Error resetFIFO();
-    Error getFIFOCount(uint16_t& count);
-    Error readFIFO(uint8_t* data, uint16_t length);
+    Error getFIFOCount(uint16_t &count);
+    Error readFIFO(uint8_t *data, uint16_t length);
 
     // To-Do Add more APIs
 
-    Error readTemperatureC(float&);
-
     Error isConnected();
 
-    Error getDeviceId(uint8_t& id);
+    Error getDeviceId(uint8_t &id);
 
     Error setSleep(bool sleep);
-
-    Error getSleep(bool&);
+    Error getSleep(bool &);
 
     Error setStandby(...);
 
     Error signalPathReset();
 
     Error setInterruptEnable();
-
     Error getInterruptStatus();
 
     Error enableTemperature();
@@ -60,33 +59,27 @@ public:
     Error getMotionStatus();
 
     Error enableInterrupt(Interrupt interrupt);
-
     Error disableInterrupt(Interrupt interrupt);
-
-    Error readInterruptStatus(uint8_t& status);
-
-
+    Error readInterruptStatus(uint8_t &status);
 
 private:
     II2C &i2c_;
     uint8_t address_;
 
     Error writeRegister(uint8_t reg, uint8_t value);
-    Error readRegister(uint8_t reg, uint8_t& value);
+    Error readRegister(uint8_t reg, uint8_t &value);
 
     Error readRegisters(uint8_t reg,
-                        uint8_t* buffer,
-                        size_t length
-);
+                        uint8_t *buffer,
+                        size_t length);
 
     // To-Do
 
-    Error readBit(uint8_t reg, uint8_t bit, bool& value);
+    Error readBit(uint8_t reg, uint8_t bit, bool &value);
 
     Error writeBit(uint8_t reg, uint8_t bit, bool value);
 
-    Error readBits(uint8_t reg, uint8_t startBit, uint8_t length, uint8_t& value);
+    Error readBits(uint8_t reg, uint8_t startBit, uint8_t length, uint8_t &value);
 
     Error writeBits(uint8_t reg, uint8_t startBit, uint8_t length, uint8_t value);
-
 };
